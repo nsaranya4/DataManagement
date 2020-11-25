5cf00e79275b8e862c42e6294f33326b52e2b887
Jerry Che <jerryche@utexas.edu>
2020-11-24 22:13:23 -0600

    added insert users into play_song_tr.sql

--- play_song_tr.sql
+++ play_song_tr.sql
@@ -34,10 +34,11 @@ CREATE TABLE song
 CREATE TABLE user_sp
 (
     user_sp_id                  NUMBER(10)          PRIMARY KEY,
-    user_sp_name                VARCHAR(50)         NOT NULL,
-    user_sp_email               VARCHAR(50)         UNIQUE,
-    user_sp_dob                 DATE                NOT NULL,
-    user_sp_subscription_type   VARCHAR(50)         NOT NULL,
+    f_name                      VARCHAR(50)         NOT NULL,
+    m_name                      VARCHAR(50)         NULL,
+    l_name                      VARCHAR(50)         NOT NULL,
+    dob                         DATE                NOT NULL,
+    email                       VARCHAR(50)         NOT NULL        UNIQUE,
     user_sp_followers           NUMBER(10)          NULL,
     user_sp_following           NUMBER(10)          NULL
 );
@@ -52,3 +53,40 @@ CREATE TABLE song_play
     CONSTRAINT song_play_song_id FOREIGN KEY(song_id) REFERENCES song(song_id),
     CONSTRAINT song_play_user_id FOREIGN KEY(user_id) REFERENCES user_sp(user_sp_id)
 );
+
+--Adding Users
+INSERT INTO user_sp 
+VALUES (1000001,'Jacque','Robert','Sawyer','18-OCT-01','j.sawyer@gmail.com');
+
+INSERT INTO user_sp(user_sp_id,f_name,l_name,dob,email) 
+VALUES (1000002,'Yvonne','Doherty','07-SEP-91','yvonne345@yahoo.com');
+
+INSERT INTO user_sp
+VALUES (1000003,'Hareem','Robert','Sawyer','03-JAN-97','h.b.heaton');
+
+INSERT INTO user_sp(user_sp_id,f_name,l_name,dob,email) 
+VALUES (1000004,'Lewie','Short','20-JAN-83','lewiethegreat@gmail.com');
+
+INSERT INTO user_sp
+VALUES (1000005,'Ada','Lewis','Simons','25-SEP-93','adaLewis@yahoo.com');
+
+INSERT INTO user_sp(user_sp_id,f_name,l_name,dob,email) 
+VALUES (1000006,'Bridget','Paine','20-FEB-94','bringthepaine@gmail.com');
+
+INSERT INTO user_sp 
+VALUES (1000007,'Reilly','Hurst','Keith','26-MAY-83','reillyhurst@outlook.com');
+
+INSERT INTO user_sp(user_sp_id,f_name,l_name,dob,email) 
+VALUES (1000008,'Karam','Derrick','04-AUG-92','karam213@aol.com');
+
+INSERT INTO user_sp 
+VALUES (1000009,'Elsa','Lily','Hopkins','27-OCT-95','elsa-lily@gmail.com');
+
+INSERT INTO user_sp(user_sp_id,f_name,l_name,dob,email) 
+VALUES (1000010,'Avni','Gates','25-JUN-01','openthegates@gmail.com');
+
+INSERT INTO user_sp(user_sp_id,f_name,l_name,dob,email) 
+VALUES (1000011,'Hisham','Rowley','15-MAR-91','rowleythetroops@gmail.com');
+
+INSERT INTO user_sp(user_sp_id,f_name,l_name,dob,email) 
+VALUES (1000012,'Nyah','Zhang','Yang','13-APR-94','nyah.yang@gmail.com');
